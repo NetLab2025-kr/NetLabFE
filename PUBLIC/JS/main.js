7150062cf0d35aa4999460f6d0f9330ee1250306
SF0629 <markvj0629@gmail.com>
2025-08-16 20:25:40 +0900

    cleanup function if check

--- PUBLIC/JS/main.js
+++ PUBLIC/JS/main.js
@@ -626,7 +626,9 @@ document.addEventListener('DOMContentLoaded', () => {
                     console.log('연결 전 임시 선 연결 해제');
                   }
                   if(tempTarget) {
-                    tempTarget.cleanupMouseMove();
+                    if(typeof tempTarget.cleanupMouseMove === "function") {
+                      tempTarget.cleanupMouseMove();
+                    }
                     tempTarget.remove();
                     tempTarget = null;
                     console.log('연결 전 임시 엔포 지워짐');
@@ -690,7 +692,9 @@ document.addEventListener('DOMContentLoaded', () => {
                   console.log('연결 후 임시 선 연결 해제');
                 }
                 if(tempTarget) {
-                  tempTarget.cleanupMouseMove();
+                  if(typeof tempTarget.cleanupMouseMove === "function") {
+                      tempTarget.cleanupMouseMove();
+                  }
                   tempTarget.remove();
                   tempTarget = null;
                   console.log('연결 후 임시 엔포 지워짐');
