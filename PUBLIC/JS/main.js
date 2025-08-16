37ce39b67c7259ac46df40d73627c59e7ca5ecd2
SF0629 <markvj0629@gmail.com>
2025-08-16 17:32:52 +0900

    jsPlumb back

--- PUBLIC/JS/main.js
+++ PUBLIC/JS/main.js
@@ -638,10 +638,22 @@ document.addEventListener('DOMContentLoaded', () => {
                   // console.log("firstClicked instanceof HTMLElement =", firstClicked instanceof HTMLElement);
                   // console.log("el instanceof HTMLElement =", el instanceof HTMLElement);
 
+                  const sourceEP = instance.addEndpoint(firstClicked, {
+                    anchors: "Center",
+                    isSource: true,
+                    isTarget: true
+                  });
+
+                  const targetEP = instance.addEndpoint(el, {
+                    anchors: "Center",
+                    isSource: true,
+                    isTarget: true
+                  });
+
 
                   instance.connect({
-                    source: firstClicked.id,
-                    target: el.id,
+                    source: sourceEP,
+                    target: targetEP,
                     connector: connectorType,
                     paintStyle: { stroke: strokeColor, strokeWidth: 4, dashstyle: dotline },
                     anchors: ["Center", "Center"],
