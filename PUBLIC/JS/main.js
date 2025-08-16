b5df8cb5cdfb4f0245072d3309d966afea08652e
SF0629 <markvj0629@gmail.com>
2025-08-16 15:29:13 +0900

    jsPlumb error sibal

--- PUBLIC/JS/main.js
+++ PUBLIC/JS/main.js
@@ -633,8 +633,8 @@ document.addEventListener('DOMContentLoaded', () => {
                   }
 
                   instance.connect({
-                    source: firstClicked,
-                    target: el,
+                    source: firstClicked.id,
+                    target: el.id,
                     connector: connectorType,
                     paintStyle: { stroke: strokeColor, strokeWidth: 4, dashstyle: dotline },
                     anchors: ["Center", "Center"],
