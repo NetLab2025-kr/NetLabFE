d5e5c189820f15460f8d5736f2061de7b0a2108d
SF0629 <markvj0629@gmail.com>
2025-08-18 01:43:58 +0900

    뭔가 수정함

--- PUBLIC/JS/main.js
+++ PUBLIC/JS/main.js
@@ -759,9 +759,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 IntBox.appendChild(clickBox);
 
               });
-                IntContainer.style.left = e.clientX + "px";
-                IntContainer.style.top = e.clientY + "px";
-                IntContainer.appendChild(IntBox);
+              IntContainer.style.left = e.clientX + "px";
+              IntContainer.style.top = e.clientY + "px";
+              IntContainer.appendChild(IntBox);
 
               selectedBox = document.querySelectorAll(`.IntBox[data-target="${el.id}"] > .clickBox`);
               topology_inner.appendChild(IntContainer);
