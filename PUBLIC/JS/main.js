a46abe2db4a1c09a38de1b317b5e58fc2d9890df
SF0629 <markvj0629@gmail.com>
2025-08-18 00:14:43 +0900

    selectedBox 선언

--- PUBLIC/JS/main.js
+++ PUBLIC/JS/main.js
@@ -762,6 +762,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 IntBox.style.top = e.clientY + "px";
               });
               topology_inner.appendChild(IntContainer);
+
+              const selectedBox =  document.querySelector(`.IntBox[data-target="${el.id}"]`);
               selectDevicePort(el.id);
 
               document.querySelectorAll(`${selectedBox} .IntBox`).addEventListener('click', () => {
